8be8480e573d73197e720765092b2f9bcff1a40e
fredegd <124848643+fredegd@users.noreply.github.com>
2023-06-25 17:03:11 +0200

    deployment

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.json());
 
 app.use("/api/books", booksRouter);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT} http://localhost:${PORT}`);
 });
